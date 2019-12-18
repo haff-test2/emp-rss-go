80aa4f2de283fc9037a6db987a9d56fd5a20574c
haff <aleksandr.s.babak@gmail.com>
2019-12-18 08:42:27 +0300

    finished main logic

--- reader/package_api.go
+++ reader/package_api.go
@@ -1,12 +1,12 @@
 package reader
 
 import (
-	// "github.com/mmcdole/gofeed"
-	// "github.com/mmcdole/gofeed/rss"
+	"github.com/mmcdole/gofeed"
 	"time"
 )
 
 type RssItem struct {
+	FeedUrl     string
 	Title       string
 	Source      string
 	SourceUrl   string
@@ -17,6 +17,47 @@ type RssItem struct {
 
 func Parse(urls []string) []RssItem {
 	var result []RssItem
+
+	ch := make(chan []RssItem)
+	for _, url := range urls {
+		go parseFeedUrl(url, ch)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		newItemsBatch := <-ch
+		result = append(result, newItemsBatch...)
+	}
+
+	return result
+}
+
+func parseFeedUrl(url string, ch chan []RssItem) {
+	var resultItems []RssItem
+
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
+	if err == nil {
+		for _, item := range feed.Items {
+			newItem := buildRssItem(*item)
+			resultItems = append(resultItems, newItem)
+		}
+	}
+
+	ch <- resultItems
+}
+
+func buildRssItem(item gofeed.Item) RssItem {
+	result := RssItem{
+		Title: item.Title,
+		// Source: ,
+		// SourceUrl:,
+		Link:        item.Link,
+		Description: item.Description,
+	}
+
+	if item.PublishedParsed != nil {
+		result.PublishDate = *(item.PublishedParsed)
+	}
 
 	return result
 }
